c407b2a24b5f0ffc93e435e4f310ea85c3b7a72b
scope1 <scope1@VITUCC.AC>
2025-03-13 15:27:47 +0530

    Added profile

---
+++ my_project/lib/profile/profile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/shared/shared_text.dart';
+import 'package:my_project/models/project.dart';
+import 'package:my_project/theme.dart';
+class Profile extends StatelessWidget {
+  const Profile({super.key,required this.project});
+  final Project project;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: StyledTitle(project.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withValues(alpha: 0.3),
+              child:Row(
+                children: [
+                  Image.asset(
+                    'assets/img/category/${project.category.image}',
+                    width: 140,
+                    height: 140,),
+                    SizedBox(width: 20,),
+                    Expanded(child:
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(project.category.title),
+                        StyledText(project.category.description)
+                      ],
+                    )
+                    )
+                ],)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
